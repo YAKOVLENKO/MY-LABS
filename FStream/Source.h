d7a40afb3c54fb966651af979d6f15c4202a915e
YAKOVLENKO <svobodinayakovlenko@yandex.ru>
2017-05-28 20:25:08 +0300

    Update Source.h

--- FStream/Source.h
+++ FStream/Source.h
@@ -27,18 +27,24 @@ public:
 
 	TFsream& operator<<(std::string str) 
 	{
+		if (file == NULL)
+			throw std::exception()
 		fprintf(file, str.c_str());
 		return *this;
 	}
 
 	TFsream& operator<<(char * str) 
 	{
+		if (file == NULL)
+			throw std::exception()
 		fprintf(file, str);
 		return *this;
 	}
 
 	TFsream& operator<<(std::ostream&(*endl)(std::ostream&))
 	{
+		if (file == NULL)
+			throw std::exception()
 		std::ostringstream ss;
 		ss << endl;
 		fprintf(file, ss.str().c_str());
