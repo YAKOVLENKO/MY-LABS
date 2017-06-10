a3d2ffc79e71baa044b2bc4e53383e86fb7e1bbc
YAKOVLENKO <svobodinayakovlenko@yandex.ru>
2017-06-10 19:25:54 +0300

    Create Addings.h

---
+++ FStream/enw/Addings.h
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+
+class TFsream
+{
+	FILE *file;
+public:
+
+
+	TFsream() = default;
+
+	TFsream(std::string str, std::string type)
+	{
+		file = fopen(str.c_str(), type.c_str());
+	};
+
+	~TFsream()
+	{
+		fclose(file);
+	};
+
+	TFsream& operator<<(std::string str)
+	{
+		fprintf(file, str.c_str());
+		return *this;
+	}
+
+	TFsream& operator<<(char * str)
+	{
+		fprintf(file, str);
+		return *this;
+	}
+
+	TFsream& operator<<(std::ostream&(*endl)(std::ostream&))
+	{
+		std::ostringstream ss;
+		ss << endl;
+		fprintf(file, ss.str().c_str());
+		return *this;
+	}
+
+	void close()
+	{
+		fclose(file);
+	}
+
+};
