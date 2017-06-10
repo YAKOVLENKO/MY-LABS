e26ea23370f30d527074141e602c3d4ee050464e
YAKOVLENKO <svobodinayakovlenko@yandex.ru>
2017-06-10 21:23:30 +0300

    Update Addings.h

--- FStream/enw/Addings.h
+++ FStream/enw/Addings.h
@@ -1,42 +1,75 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <string>
 #include <iostream>
 #include <sstream>
-
-
+#include <stdio.h>
 class TFsream
 {
 	FILE *file;
+	bool opened = 0;
 public:
-
 
 	TFsream() = default;
 
 	TFsream(std::string str, std::string type)
 	{
 		file = fopen(str.c_str(), type.c_str());
+		if (file != NULL)
+			opened = 1;
 	};
+
+	void open(const char * str, const char *  type)
+	{
+		if (opened) close();
+		file = fopen(str, type);
+		if (file != NULL)
+			opened = 1;
+	}
+
+	bool is_open()
+	{
+		return opened;
+	}
 
 	~TFsream()
 	{
-		fclose(file);
+		if (!opened) fclose(file);
+		opened = 0;
 	};
 
 	TFsream& operator<<(std::string str)
 	{
+		if (!opened)
+			throw std::exception();
 		fprintf(file, str.c_str());
 		return *this;
 	}
 
-	TFsream& operator<<(char * str)
+	TFsream& operator >> (char *& str)
 	{
-		fprintf(file, str);
+
+		if (!opened)
+			throw std::exception();
+		fscanf_s(file, "%s", str);
 		return *this;
 	}
 
+	TFsream& operator >> (std::string& str)
+	{
+
+		char * tmp = new char[1024];
+		if (!opened)
+			throw std::exception();
+		fscanf_s(file, "%s", tmp);
+		str = tmp;
+		return *this;
+	}
+
+
 	TFsream& operator<<(std::ostream&(*endl)(std::ostream&))
 	{
+		if (!opened)
+			throw std::exception();
 		std::ostringstream ss;
 		ss << endl;
 		fprintf(file, ss.str().c_str());
@@ -45,7 +78,51 @@ public:
 
 	void close()
 	{
-		fclose(file);
+		if (opened) fclose(file);
+		opened = 0;
+	}
+
+	std::string getline1()
+	{
+		if (!opened) return "";
+		char * line = NULL;
+		size_t len = 0;
+		ssize_t read;
+		read = getline(&line, &len, file);
+		std::string tmp(line);
+		return tmp;
+	}
+
+	std::string read()
+	{
+		if (!opened)
+			return "";
+		fseek(file, 0, SEEK_END);
+		int size = ftell(file);
+		fseek(file, 0, SEEK_SET);
+		char * fcontent = new char[size];
+		fread(fcontent, 1, size, file);
+		std::string tmp(fcontent);
+		delete[] fcontent;
+		return tmp;
+	}
+
+	std::string write(std::string s)
+	{
+		if (opened)
+			fprintf(file, s.c_str());
+	}
+
+	void seek(int pos)
+	{
+		if (opened)
+			fseek(file, pos, SEEK_SET);
+	}
+
+
+	int eof()
+	{
+		return feof(file);
 	}
 
 };
